cb4dcf81932d7ced9cf07f52368cc3d5b6615d12
Jorge Medina <medidrones@gmail.com>
2020-12-05 01:46:36 -0300

    Implementado na API.Service e API.Service.Test(UF, Municipio e CEP)

--- src/Api.CrossCutting/Mappings/DtoToModelProfile.cs
+++ src/Api.CrossCutting/Mappings/DtoToModelProfile.cs
@@ -1,6 +1,9 @@
 using AutoMapper;
 using Api.Domain.Dtos.User;
 using Api.Domain.Models;
+using Api.Domain.Dtos.Uf;
+using Api.Domain.Dtos.Municipio;
+using Api.Domain.Dtos.Cep;
 
 namespace Api.CrossCutting.Mappings
 {
@@ -8,9 +11,27 @@ namespace Api.CrossCutting.Mappings
     {
         public DtoToModelProfile()
         {
+            #region User
             CreateMap<UserModel, UserDto>().ReverseMap();
             CreateMap<UserModel, UserDtoCreate>().ReverseMap();
             CreateMap<UserModel, UserDtoUpdate>().ReverseMap();
+            #endregion
+
+            #region UF
+            CreateMap<UfModel, UfDto>().ReverseMap();
+            #endregion
+
+            #region Municipio
+            CreateMap<MunicipioModel, MunicipioDto>().ReverseMap();
+            CreateMap<MunicipioModel, MunicipioDtoCreate>().ReverseMap();
+            CreateMap<MunicipioModel, MunicipioDtoUpdate>().ReverseMap();
+            #endregion
+
+            #region CEP
+            CreateMap<CepModel, CepDto>().ReverseMap();
+            CreateMap<CepModel, CepDtoCreate>().ReverseMap();
+            CreateMap<CepModel, CepDtoUpdate>().ReverseMap();
+            #endregion
         }
     }
 }
--- src/Api.CrossCutting/Mappings/EntityToDtoProfile.cs
+++ src/Api.CrossCutting/Mappings/EntityToDtoProfile.cs
@@ -1,6 +1,9 @@
 using AutoMapper;
 using Api.Domain.Dtos.User;
 using Api.Domain.Entities;
+using Api.Domain.Dtos.Uf;
+using Api.Domain.Dtos.Municipio;
+using Api.Domain.Dtos.Cep;
 
 namespace Api.CrossCutting.Mappings
 {
@@ -8,9 +11,28 @@ namespace Api.CrossCutting.Mappings
     {
         public EntityToDtoProfile()
         {
+            #region User
             CreateMap<UserDto, UserEntity>().ReverseMap();
             CreateMap<UserDtoCreateResult, UserEntity>().ReverseMap();
             CreateMap<UserDtoUpdateResult, UserEntity>().ReverseMap();
+            #endregion
+
+            #region UF
+            CreateMap<UfDto, UfEntity>().ReverseMap();
+            #endregion
+
+            #region Municipio
+            CreateMap<MunicipioDto, MunicipioEntity>().ReverseMap();
+            CreateMap<MunicipioDtoCompleto, MunicipioEntity>().ReverseMap();
+            CreateMap<MunicipioDtoCreateResult, MunicipioEntity>().ReverseMap();
+            CreateMap<MunicipioDtoUpdateResult, MunicipioEntity>().ReverseMap();
+            #endregion
+
+            #region CEP
+            CreateMap<CepDto, CepEntity>().ReverseMap();
+            CreateMap<CepDtoCreateResult, CepEntity>().ReverseMap();
+            CreateMap<CepDtoUpdateResult, CepEntity>().ReverseMap();
+            #endregion
         }
     }
 }
